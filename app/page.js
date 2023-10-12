f012af96f77c05f281d4a99dd4928f9225888a7c
Dylan <dylansy123@gmail.com>
2023-10-11 22:27:50 -0500

    substack

--- app/page.js
+++ app/page.js
@@ -271,6 +271,14 @@ export default function Home() {
         >
           Twitter
         </a>
+        <a
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://dylansy.substack.com/"
+        >
+          Substack
+        </a>
       </div>
     </main>
   );
