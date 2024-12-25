f7186d43e8dcdcb7486ab9956fa19f0e302d4534
Dylan <dylansy123@gmail.com>
2024-12-25 12:54:13 -0600

    massive revamp 2025

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
-import Head from "next/head";
-import "../public/style.css"; // Import your CSS file
-import React from "react"; // Make sure to import React
+import "../public/style.css";
+import React from "react";
 
 export default function Home() {
   return (
@@ -8,142 +7,15 @@ export default function Home() {
       <div className="flex-col items-center justify-center mt-8 lg:grid-cols-2">
         <div>
           <h1 className="mt-5 text-5xl font-extrabold leading-[1.15] text-black sm:text-3xl">
-            Hi. I'm Dylan Sy,{" "}
+            dylan sy
           </h1>
-          <h1 className="mt-5 text-5xl font-extrabold leading-[1.15] text-black sm:text-3xl">
-            I'm a software engineer.{" "}
-          </h1>
-        </div>
-
-        <div className="mt-8 grid lg:mb-0 w-full lg:text-left max-w-[55ch]">
-          <h2 className={`mb-2 text-2xl font-bold underline`}>
-            What I'm Currently Working On
+          <h2 className="mt-1  font-extrabold leading-[1.15] text-black ">
+            software engineer{" "}
           </h2>
-          <a
-            href="https://www.youtube.com/@dylansy"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-2 text-2xl font-semibold`}>
-              My YouTube Channel
-            </h2>
-            <p className={`m-0 max-w-[55ch] text-sm opacity-50`}>
-              I post stuff on the internet. september 11, 2023 - current
-            </p>
-          </a>
-          <a
-            href="https://twitter.com/cohort2303"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-2 text-2xl font-semibold`}>Cohort 2303</h2>
-            <p className={`m-0 max-w-[55ch] text-sm opacity-50`}>
-              I built and run a community of builders, and graduates of
-              Fullstack Academy, Cohort 2303. march 1, 2023 - current
-            </p>
-          </a>
         </div>
-
-        {/* <div className="mt-8 grid lg:mb-0 w-full lg:text-left max-w-[55ch]">
-          <h2 className={`mb-2 text-2xl font-bold underline`}>Projects</h2>
-          <a
-            href="https://genphrase.com"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="flex">
-              <div>
-                <h2 className={`mb-2 text-2xl font-semibold`}>genphrase.com</h2>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50 mr-10`}>
-                  Generate powerful random passphrases. Built in nights &
-                  weekends by buildspace. A school for people to work on ideas
-                  they're passionate about in 6 weeks. Backed by a16z + YC.
-                  august 23, 2023
-                </p>
-              </div>
-              <img
-                src="/genphrase.jpg"
-                alt="Genphrase"
-                className="max-w-full h-auto w-32 sm:w-48 object-cover"
-              />
-            </div>
-          </a>
-
-          <a
-            href="https://github.com/sydylan/sigmatime"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="flex">
-              <div>
-                <h2 className={`mb-2 text-2xl font-semibold`}>SigmaTime </h2>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50 mr-10`}>
-                  SigmaTime is a productivity tool that offers a customizable
-                  Pomodoro timer, complete with inspirational quotes and
-                  soothing sounds, helping you stay focused and productive
-                  during work and study sessions. august 8, 2023
-                </p>
-              </div>
-              <img
-                src="/SigmaTime.jpg"
-                alt="SigmaTime Project"
-                className="max-w-full h-auto w-32 sm:w-48 object-cover"
-              />
-            </div>
-          </a>
-          <a
-            href="https://github.com/Dynamo-Dark-Aid/GoVibeFE"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="flex">
-              <div>
-                <h2 className={`mb-2 text-2xl font-semibold`}>Go Vibe </h2>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50 mr-10`}>
-                  Go Vibe is an adventurous app that presents five thrilling
-                  attractions with a single tap, offering users unforgettable
-                  experiences and opportunities to explore their current
-                  location. july 21, 2023
-                </p>
-              </div>
-              <img
-                src="/goVibe.jpg"
-                alt="Go Vibe Project"
-                className="max-w-full h-auto w-32 sm:w-48 pr-20 object-cover"
-              />
-            </div>
-          </a>
-          <a
-            href="https://twitter.com/boosted_odds"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="flex">
-              <div>
-                <h2 className={`mb-2 text-2xl font-semibold`}>Boosted Odds </h2>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50 mr-10`}>
-                  Finding +EV and Arbitrage Bets so you don't have to. I worked
-                  at Boosted Odds as a Social Media Manager and took this from
-                  Zero to One. july 21, 2023
-                </p>
-              </div>
-              <img
-                src="/boostedodds.jpg"
-                alt="Genphrase"
-                className="max-w-full h-auto w-32 sm:w-48 object-cover"
-              />
-            </div>
-          </a>
-        </div> */}
 
         <div>
-          <h2 className={`mt-8 text-2xl font-semibold underline`}>About Me</h2>
+          <h2 className={`mt-8 text-2xl font-semibold`}>some tweets</h2>
           <blockquote className="twitter-tweet">
             <p lang="en" dir="ltr">
               Hi everyone!
@@ -168,12 +40,6 @@ export default function Home() {
               July 6, 2023
             </a>
           </blockquote>
-        </div>
-
-        <div>
-          <h2 className={`mt-8 text-2xl font-semibold underline`}>
-            My time at buildspace s4{" "}
-          </h2>
           <blockquote className="twitter-tweet">
             <p lang="en" dir="ltr">
               Last weekend at s4 irl
@@ -200,17 +66,21 @@ export default function Home() {
               September 28, 2023
             </a>
           </blockquote>
-          <script
-            async
-            src="https://platform.twitter.com/widgets.js"
-            charset="utf-8"
-          ></script>
-        </div>
 
-        <h2 className={`mt-8 text-2xl font-semibold underline`}>
-          I Tweeted Everyday During My Bootcamp
-        </h2>
-        <div>
+          <blockquote className="twitter-tweet">
+            <p lang="en" dir="ltr">
+              Go Vibe.
+              <br />
+              <br />
+              Where spontaneity meets adventure.{" "}
+              <a href="https://t.co/lj8Oi6YB89">pic.twitter.com/lj8Oi6YB89</a>
+            </p>
+            &mdash; Dylan Sy (@dylansyyyy){" "}
+            <a href="https://twitter.com/dylansyyyy/status/1681806946255421442?ref_src=twsrc%5Etfw">
+              July 19, 2023
+            </a>
+          </blockquote>
+
           <blockquote className="twitter-tweet">
             <p lang="en" dir="ltr">
               Hi everyone!
@@ -249,7 +119,7 @@ export default function Home() {
           <script
             async
             src="https://platform.twitter.com/widgets.js"
-            charSet="utf-8"
+            charset="utf-8"
           ></script>
         </div>
       </div>
@@ -261,7 +131,7 @@ export default function Home() {
           rel="noopener noreferrer"
           href="https://github.com/sydylan"
         >
-          GitHub
+          github
         </a>
         <a
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -269,7 +139,7 @@ export default function Home() {
           rel="noopener noreferrer"
           href="https://www.linkedin.com/in/dylan-sy/"
         >
-          LinkedIn
+          linkedin
         </a>
         <a
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -277,7 +147,7 @@ export default function Home() {
           rel="noopener noreferrer"
           href="https://twitter.com/dylansyyyy"
         >
-          Twitter
+          twitter
         </a>
 
         <a
@@ -286,7 +156,7 @@ export default function Home() {
           rel="noopener noreferrer"
           href="https://dylansy.substack.com/"
         >
-          Substack
+          substack
         </a>
       </div>
     </main>
